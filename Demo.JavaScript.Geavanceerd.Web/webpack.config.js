ed5b34c0d9f48ef69aed86e043127c905f0227bc
David Urting <david.urting@live.com>
2020-12-05 17:42:24 +0100

    Add project files.

---
+++ Demo.JavaScript.Geavanceerd.Web/webpack.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    entry: {
+        basisClassesModules: './src/index.js'
+    },
+    output: {
+        filename: '../wwwroot/js/types-asynchronity-dom-bundle.js'
+    },
+    devtool: "source-map"
+};
